34eca5227b5621c7d81096e1ed31a5d0dcafbb5d
Mieszko Gąska <59863187+Mieszko46@users.noreply.github.com>
2022-02-18 02:49:34 +0100

    Add charades files

---
+++ Charades/server.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdbool.h>
+// #include <time.h>
+#include "string.c"
+
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 2048
+#define NAME_SIZE 32
+
+// Client structure
+typedef struct clients
+{
+    struct sockaddr_in address;
+    int sockfd;
+    int uid;
+    char name[NAME_SIZE];
+    bool wasHost;
+    int points;
+} client;
